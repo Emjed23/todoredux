13afea1886c21d7c7b97a61a3650cc27e0f29b83
Emjed23 <harabiamjed@gmail.com>
2024-01-19 17:15:21 +0100

    Tasktodo2

--- src/components/ListTodo.js
+++ src/components/ListTodo.js
@@ -30,36 +30,3 @@ const ListTodo = () => {
   }
 
 export default ListTodo;
-// import React from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Card from 'react-bootstrap/Card';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { deleteTodo } from '../redux/action/todoAction';
-// import EditTodo from './EditTodo';
-
-// const ListTodo = (el) => {
-//   const { todos } = useSelector(state => state);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <div className='alltodo'>
-//       {todos.map((item) => (
-//         <Card key={item.Task} className='card'>
-//           <Card.Body>
-//             <Card.Title>{item.Task}</Card.Title>
-//             <Card.Text>{item.description}</Card.Text>
-//             <EditTodo el={item} />
-//             <Button
-//               variant="danger"
-//               onClick={() => dispatch(deleteTodo({id:el.task,description:todos}))} // Assuming item.Task is the correct identifier
-//             >
-//               Delete
-//             </Button>
-//           </Card.Body>
-//         </Card>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default ListTodo;
